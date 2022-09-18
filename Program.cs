6addc4a9428027ba08cc78df7d382d59b2d574c1
Vera <imbesom@mail.ru>
2022-09-18 20:48:18 +0300

    дз 3

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите любое число");
+int a = Convert.ToInt32(Console.ReadLine());
+if(a%2 == 1)
+{
+Console.WriteLine(a + " "+"- нечётное");
+}
+else
+{
+Console.WriteLine(a +" "+"- чётное");
+}
+
